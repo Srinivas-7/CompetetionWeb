e1bf3d10303bfbeb5efd11bcb9eb056965e7b405
Srinivas D A <141989357+Srinivas-7@users.noreply.github.com>
2026-09-15 22:21:13 +0530

    Correct name spelling for pandhal-15

--- src/data/pandhals.js
+++ src/data/pandhals.js
@@ -576,7 +576,7 @@ export const PANDHALS_DATA = [
   {
     "id": "pandhal-15",
     "number": 15,
-    "name": "Ekanath yuvaka Samithi",
+    "name": "Ekatha yuvaka Samithi",
     "photos": [
       {
         "id": "pandhal-15-img-1",
